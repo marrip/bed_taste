1b7a0ffb8212c1316bd85b53b41b15315599b412
marrip <martin.rippin@scilifelab.uu.se>
2021-05-18 16:39:43 +0200

    Chore: Add new struct types

--- types.go
+++ types.go
@@ -1,5 +1,20 @@
 package main
 
+type Session struct {
+	Padding    int64
+	PathCna    string
+	PathProbes string
+}
+
+type RegionInfo struct {
+	ID      string
+	Gene    string
+	GeneID  string
+	TransID string
+	ExonID  string
+	Region  Region
+}
+
 type Region struct {
 	Chr   string
 	Start int64
