57d0657b9aa7efd7df95c45ef9e2fabf9694478f
marrip <martin.rippin@scilifelab.uu.se>
2021-05-19 16:48:42 +0200

    Feat: String functions together

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-// Create session struct to set genome map
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	getFlags()
+	var s Session
+	err := s.getFlags()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	data, err := s.data2CombinedRegions()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	fmt.Println(data)
+}
+
+func (s Session) data2CombinedRegions() (completed []RegionInfo, err error) {
+	probes, err := getTsvData(s.PathProbes)
+	if err != nil {
+		return
+	}
+	regions, err := getGenRegTsvData(s.PathGenReg)
+	if err != nil {
+		return
+	}
+	processed, err := s.matchProbeToRegion(regions, probes)
+	if err != nil {
+		return
+	}
+	completed = addRemainGenReg(processed, regions)
+	return
 }
