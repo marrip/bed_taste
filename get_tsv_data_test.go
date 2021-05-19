8f0991ab3df89a3485550d55f46a825ccb719362
marrip <martin.rippin@scilifelab.uu.se>
2021-05-19 08:32:18 +0200

    Test: Add tests for addData functions

--- get_tsv_data_test.go
+++ get_tsv_data_test.go
@@ -53,6 +53,115 @@ func TestReadTsv(t *testing.T) {
 	}
 }
 
+func TestAddDataToRegionInfo(t *testing.T) {
+	var cases = map[string]struct {
+		input   []string
+		output  RegionInfo
+		wantErr bool
+	}{
+		"Successfully add data to region info": {
+			[]string{
+				"ENSG00000147883",
+				"ENST00000380142",
+				"CDKN2B",
+				"2",
+				"9",
+				"22004748",
+				"22006247",
+			},
+			RegionInfo{
+				ID:      "CDKN2B_2",
+				Gene:    "CDKN2B",
+				GeneID:  "ENSG00000147883",
+				TransID: "ENST00000380142",
+				ExonID:  "2",
+				Region: Region{
+					Chr:   "9",
+					Start: 22004748,
+					End:   22006247,
+				},
+			},
+			false,
+		},
+		"Row is too short": {
+			[]string{
+				"ENSG00000147883",
+				"CDKN2B",
+				"2",
+				"9",
+				"22004748",
+				"22006247",
+			},
+			RegionInfo{
+				ID:      "",
+				Gene:    "",
+				GeneID:  "",
+				TransID: "",
+				ExonID:  "",
+				Region: Region{
+					Chr:   "",
+					Start: 0,
+					End:   0,
+				},
+			},
+			false,
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			result, err := addDataToRegionInfo(c.input)
+			checkError(t, err, c.wantErr)
+			if diff := deep.Equal(result, c.output); diff != nil {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestAddDataToRegion(t *testing.T) {
+	var cases = map[string]struct {
+		input   []string
+		output  Region
+		wantErr bool
+	}{
+		"Successfully add data to region": {
+			[]string{
+				"9",
+				"22008675",
+				"22009272",
+			},
+			Region{
+				Chr:   "9",
+				Start: 22008675,
+				End:   22009272,
+			},
+			false,
+		},
+		"Failing at adding data to region": {
+			[]string{
+				"9",
+				"one",
+				"two",
+			},
+			Region{
+				Chr:   "9",
+				Start: 0,
+				End:   0,
+			},
+			true,
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			result, err := addDataToRegion(c.input)
+			checkError(t, err, c.wantErr)
+			if diff := deep.Equal(result, c.output); diff != nil {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
 func TestCheckRow(t *testing.T) {
 	var cases = map[string]struct {
 		input   []string
@@ -96,7 +205,7 @@ func TestCheckRow(t *testing.T) {
 		"Row contains empty fields at required positions": {
 			[]string{
 				"ENSG00000147883",
-				"ENSV00000380142",
+				"ENST00000380142",
 				"CDKN2B",
 				"1",
 				"9",
@@ -108,7 +217,7 @@ func TestCheckRow(t *testing.T) {
 		"Row does not contain numbers at required positions": {
 			[]string{
 				"ENSG00000147883",
-				"ENSV00000380142",
+				"ENST00000380142",
 				"CDKN2B",
 				"one",
 				"9",
